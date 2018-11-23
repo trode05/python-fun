e1f7f8e726672be367655df5603a840bfa2b899e
Tronin, Denis <Denis.Tronin@ca.com>
2018-11-23 16:02:02 +0100

    removed log

--- bot.py
+++ bot.py
@@ -14,9 +14,6 @@ def webhook():
     req = request.get_json(force=True)
     intent = req.get('queryResult').get('intent').get('displayName')
 
-    log.error('Unexpected action requested: %s', json.dumps(req))
-
-
     # Check if the request is for the translate action
     if intent == 'pr.state':
         pr =  req['queryResult']['parameters'].get('number-integer')
