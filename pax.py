2801d71e7850891c9a02cb102604e0f3bb32f697
Tronin, Denis <Denis.Tronin@ca.com>
2018-12-06 15:17:30 +0100

    Bug fixing

--- pax.py
+++ pax.py
@@ -4,6 +4,9 @@
 #
 from pybright.cli import bright
 
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
+
 def get_elements(sub, env, stage, debug=False):
     command = f"endevor list elements --sys AML020 --type * --env {env} --sn {stage}"
     if(sub!=""):
@@ -20,11 +23,15 @@ def get_elements(sub, env, stage, debug=False):
         print('No elements found')
         return None
 
-def promote_elements(elements):
-    for element in elements:
-        if(element['typeName']!="SANDBOX"):
-            command = f">> bright endevor move ele {element['elmName']} --type {element['typeName']} --sn {element['stgNum']}"
-        print(command)  
+def promote_elements(elements, comment="pax automation", ccid="pax"):
+    if(elements):
+        for el in elements:
+            if(el['typeName']!="SANDBOX"):
+                command = f">> bright endevor move ele {el['elmName']} --type {el['typeName']} --sn {el['stgNum']} --sys {el['sysName']} --cci {ccid} --com \"{comment}\" --sm"
+                if(el['sbsName']):
+                    command = f"{command} --sub {el['sbsName']}"
+            print(command)  
+            bright(command)
 
 promote_elements(get_elements("AML02001", "DEV", 1))
 promote_elements(get_elements("AML02001", "DEV", 2))
